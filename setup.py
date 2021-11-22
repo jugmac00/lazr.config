936b65b7b5af6185f6c3948920554facb192cd6e
Colin Watson <cjwatson@debian.org>
2021-11-22 13:02:12 +0000

    Update Python versions
    Add support for Python 3.9 and 3.10.  Drop support for Python 3.4.

--- setup.py
+++ setup.py
@@ -62,11 +62,12 @@ files. The format supports schema validation.
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         ],
     extras_require=dict(
         docs=['Sphinx'],
